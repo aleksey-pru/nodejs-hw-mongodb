83ac9922ab959c44c0c7911e2b113b78ed98c251
Oleksii <o.pruzhanskiy@gmail.com>
2025-06-06 15:54:15 +0300

    final

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import { initMongoConnection } from './db/initMongoConnection.js';
+import { setupServer } from './server.js';
+
+const bootsrap = async () => {
+  await initMongoConnection();
+  setupServer();
+};
+
+bootsrap();
---
+++ src/services/contacts.js
@@ -0,0 +1,12 @@
+import { ContactsCollection } from '../db/models/contacts.js';
+
+export const getAllContacts = async () => {
+  const contacts = await ContactsCollection.find();
+  console.log(contacts);
+  return contacts;
+};
+
+export const getContactById = async (contactId) => {
+  const contacts = await ContactsCollection.findById(contactId);
+  return contacts;
+};
