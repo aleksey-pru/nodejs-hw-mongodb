4a4a0f0aa7ea8e458ea1a177d2709b84f47b06e6
Oleksii <o.pruzhanskiy@gmail.com>
2025-06-13 13:53:44 +0300

    CRUD

--- src/server.js
+++ src/server.js
@@ -2,7 +2,9 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import { getEnvVar } from './utils/getEnvVar.js';
-import { getAllContacts, getContactById } from './services/contacts.js';
+import router from './routers/contacts.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { errorHandler } from './middlewares/errorHandler.js';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 
@@ -20,37 +22,10 @@ export const setupServer = () => {
     }),
   );
 
-  app.get('/contacts', async (req, res) => {
-    res.status(200).json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data: await getAllContacts(),
-    });
-  });
+  app.use(router);
+  app.use(notFoundHandler);
+  app.use(errorHandler);
 
-  app.get('/contacts/:contactId', async (req, res) => {
-    const { contactId } = req.params;
-
-    const contact = await getContactById(contactId);
-    if (!contact) {
-      res.status(404).json({
-        status: 404,
-        message: 'Contact not found!',
-      });
-      return;
-    }
-    res.status(200).json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data: contact,
-    });
-  });
-
-  app.use((req, res) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  });
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -10,3 +10,25 @@ export const getContactById = async (contactId) => {
   const contacts = await ContactsCollection.findById(contactId);
   return contacts;
 };
+
+export const createContact = async (payload) => {
+  const contact = await ContactsCollection.create(payload);
+  return contact;
+};
+
+export const deleteContact = async (contactId) => {
+  const contact = await ContactsCollection.findByIdAndDelete(contactId);
+  return contact;
+};
+
+export const patchContact = async (contactId, payload) => {
+  const updatedContact = await ContactsCollection.findOneAndUpdate(
+    {
+      _id: contactId,
+    },
+    { $set: payload },
+    { new: true },
+  );
+  if (!updatedContact) return null;
+  return { contact: updatedContact };
+};
