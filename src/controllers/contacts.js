fc57f4b616491bffe3929fe957abfd811a865ca5
Oleksii <o.pruzhanskiy@gmail.com>
2025-07-05 10:40:26 +0300

    fix cloudinary for post

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -67,9 +67,19 @@ export const getContactByIdController = async (req, res, next) => {
 
 export const createContactController = async (req, res) => {
   const { _id: userId } = req.user;
+  const photo = req.file;
+  let photoUrl;
+  if (photo) {
+    if (getEnvVar('ENABLE_CLOUDINARY') === 'true') {
+      photoUrl = await saveFileToCloudinary(photo);
+    } else {
+      photoUrl = await saveFileToUploadDir(photo);
+    }
+  }
   const contact = await createContact({
     ...req.body,
     userId,
+    photo: photoUrl,
   });
   res.status(201).json({
     status: 201,
